8bf1143cfd3bb36f17748365652bc77e1fa9c457
AlanAlv <alan.alv96@gmail.com>
2020-03-15 19:07:03 -0600

    Find user

--- app/src/main/java/com/example/channel/Controller/LoginActivity.kt
+++ app/src/main/java/com/example/channel/Controller/LoginActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.channel.R
+import com.example.channel.Services.AuthService
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
@@ -14,7 +16,18 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginLoginBtnClicked(view: View){
+        val email = loginEmailTxt.text.toString()
+        val password = loginPasswordText.text.toString()
 
+        AuthService.logibUser(this, email, password) {loginSuccess ->
+            if (loginSuccess) {
+                AuthService.findUserByEmail(this) {findSuccess ->
+                    if (findSuccess) {
+                        finish()
+                    }
+                }
+            }
+        }
     }
 
     fun loginCreateUserBtnClicked(view: View){
